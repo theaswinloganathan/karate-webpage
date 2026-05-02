13053038555d315fb9daace4297421b80324b47d
Aswin Loganathan <theaswinloganathan@gmail.com>
2026-05-02 12:31:16 +0530

    Add global error debugger

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Global error handler for demo debugging
+window.onerror = function(msg, url, line) {
+  alert("Vite Error: " + msg + "\nLine: " + line);
+  return false;
+};
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
